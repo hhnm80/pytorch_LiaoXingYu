0b28dc3b023f5d93278ba2c42b78445538c0f0b2
Administrator <1280242622@qq.com>
2022-06-12 18:40:54 +0800

    1`2`1

--- 求梯度练习.py
+++ 求梯度练习.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import torch
 from torch.autograd import Variable
+
+# Variable函数的作用是将torch.FloatTensor([2, 3])变成Variable变量,Variable变量是神经网络里面特有的一种数据结构,Variable函数的作用就是将张量转换为Variable变量,,,,
 x = Variable(torch.FloatTensor([2, 3]), requires_grad=True)
 
 # 先是定义了,k的形状和值,k的形状,zeros这个函数在很多地方都有,numpy里面有,matlab里面有,在numpy里面,zeros函数就是创建一个数组,这个数组的"质"已经规定了,就是里面的元素全为0 ,但是还没有"形",因此我们传入参数,一个或者两个或者三个参数,几个参数就是几维,这里传入一个2,就是1个维度,而且这个维度的元素个数是2,注意这个2不是数值,因为数值已经给你规定了,有几个维度还不够,还要知道每个维度有多少个数值,在这里,一个参数就是说明只有一个维度,这个维度的元素个数是2,值全为0,简单表示就是[0,0],在pytorch里面,函数的调用返回值一般都是tensor张量,没有数组的概念,torch.zeros(2)不好理解???zeros(2)好理解吧,一个函数,参数为2....
@@ -42,7 +44,7 @@ print("x梯度形状的值是:",x.grad.shape)
 
 # zero_()函数
 
-#
+#对x的梯度值归0
 x.grad.data.zero_() # 归零之前求得的梯度
 
 #
@@ -55,7 +57,9 @@ print("x的梯度的数据",x.grad.data)
 # 在这里，我们再次执行求导函数，k调用求导函数，是对k里面的变量x求导，由于上面已经保留了计算图，所以在这里，注意导数值的累加问题。。。。具体我也不明白为什么要累加，可能是要对计算图进行进一步的了解。。。。。。。
 k.backward(torch.FloatTensor([0, 1]))
 
+# 注意求导的是
 
+# x的梯度 tensor([2., 6.])，为什么是这个值呢？？？？假设不对梯度归零,值为多少呢?????
 print("x的梯度",x.grad)
 
 
